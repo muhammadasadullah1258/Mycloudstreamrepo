5266cfe561586d8c973e26c055e58f4046ed7bd9
muhammadasadullah1258 <muhammadasadullah125846@gmail.com>
2026-01-16 16:29:45 +0500

    Update NetMirrorPlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/NetMirrorPlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/NetMirrorPlugin.kt
@@ -1,1 +1,11 @@
+package com.example
 
+import com.lagradost.cloudstream3.CloudstreamPlugin
+import android.content.Context
+
+class NetMirrorPlugin: CloudstreamPlugin() {
+    override fun load(context: Context) {
+        // Sirf ye line provider ko app mein register karti hai
+        registerMainAPI(NetMirrorProvider())
+    }
+}
