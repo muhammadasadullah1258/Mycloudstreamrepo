801160ce311e30efda4909ca4b2cb4908ef21958
muhammadasadullah1258 <muhammadasadullah125846@gmail.com>
2026-01-16 15:06:11 +0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        // Stable Android Gradle Plugin (Cloudstream compatible)
+        classpath("com.android.tools.build:gradle:8.2.2")
+
+        // Cloudstream Gradle Plugin
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+
+        // Stable Kotlin (Cloudstream supported)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        // Tumhara GitHub repo (correct)
+        setRepo("muhammadasadullah1258/Mycloudstreamrepo")
+    }
+
+    android {
+        namespace = "com.muhammadasadullah.cloudstream"
+
+        // compileSdk yahan hota hai
+        compileSdk = 35
+
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 35
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+
+        tasks.withType<KotlinJvmCompile> {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_1_8)
+            }
+        }
+    }
+
+    dependencies {
+        val cloudstream by configurations
+        val implementation by configurations
+
+        // Cloudstream API
+        cloudstream("com.lagradost:cloudstream3:pre-release")
+
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.11")
+        implementation("org.jsoup:jsoup:1.18.3")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    }
+}
