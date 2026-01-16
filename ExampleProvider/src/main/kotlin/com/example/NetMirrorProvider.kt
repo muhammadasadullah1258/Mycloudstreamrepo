d8f7e0efb8ffce24c2cf5791a4a154ad336c8ec3
muhammadasadullah1258 <muhammadasadullah125846@gmail.com>
2026-01-16 16:30:13 +0500

    Update NetMirrorProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/NetMirrorProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/NetMirrorProvider.kt
@@ -1,1 +1,26 @@
+package com.example
 
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import org.jsoup.Jsoup
+
+class NetMirrorProvider : MainAPI() { // Provider hamesha MainAPI hota hai
+    override var name = "NetMirror"
+    override var mainUrl = "https://netmirror2.bio"
+    override val supportedTypes = setOf(TvType.Movie)
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val url = "$mainUrl/search?q=$query"
+        val response = app.get(url).text
+        val document = Jsoup.parse(response)
+        
+        return document.select("a.movie-link").map {
+            val title = it.text()
+            val href = it.attr("href")
+            // CloudStream search responses is format mein hoti hain
+            newMovieSearchResponse(title, href, TvType.Movie) {
+                this.posterUrl = "" // Poster ka link yahan ayega
+            }
+        }
+    }
+}
